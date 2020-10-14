ca97d1ae7f603fd672dd532baa780ad14b8d89ec
Ersin Elci <ersinelci@gmail.com>
2020-10-14 15:37:30 +0300

    Update payingoffdebtyear.py

--- payingoffdebtyear.py
+++ payingoffdebtyear.py
@@ -1,14 +1,15 @@
-#Write a program to calculate the credit card balance after one year if a person only pays the minimum monthly payment required by the credit card company each month.
-#The following variables contain values as described below:
+'''
+Write a program to calculate the credit card balance after one year if a person only pays the minimum monthly payment required by the credit card company each month.
+The following variables contain values as described below:
 
-#balance - the outstanding balance on the credit card
+balance - the outstanding balance on the credit card
 
-#annualInterestRate - annual interest rate as a decimal
+annualInterestRate - annual interest rate as a decimal
 
-#monthlyPaymentRate - minimum monthly payment rate as a decimal
+monthlyPaymentRate - minimum monthly payment rate as a decimal
 
-#For each month, calculate statements on the monthly payment and remaining balance. At the end of 12 months, print out the remaining balance.
-
+For each month, calculate statements on the monthly payment and remaining balance. At the end of 12 months, print out the remaining balance.
+'''
 def balancecalculator(balance,annualInterestRate,monthlyPaymentRate):
    
     
